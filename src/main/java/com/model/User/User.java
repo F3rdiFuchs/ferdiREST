042af587ff6ff66c658dd17ec05d7d3da6dc5a4e
F3rdiFuchs <f.bau3r@gmail.com>
2015-09-10 17:30:55 +0200

    new

---
+++ src/main/java/com/model/User/User.java
@@ -0,0 +1,50 @@
+package com.model.User;
+
+public class User {
+	private int userId;
+	private String userName;
+	private String name;
+	private String lastName;
+	private String email;
+	private String password;
+	
+	public User(String username) {
+		this.userName = username;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return userName;
+	}
+	public void setUsername(String username) {
+		this.userName = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
